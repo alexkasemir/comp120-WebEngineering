6d47e8d7047812c17b544d50179423ff56e53555
Abdisalan <abdisalan.mohamud@tufts.edu>
2015-02-09 17:09:21 -0500

    added amazon s3 stuff almost done

--- djangoapp/purrer/settings.py
+++ djangoapp/purrer/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'meows',
+    'storages',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,3 +83,36 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#########################################
+#    AMAZON S3 WEB SERVICES SETTINGS    #
+#########################################
+
+#This makes sure that wehn files are uploaded to S3, browsers are given
+#an HTTP header in the response. The headers in turn will tell
+#browsers that they can cache these files for a very long time
+
+AWS_HEADERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 EST',
+    'Cache-Control': 'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = secrets.BUCKET_NAME
+AWS_ACCESS_KEY_ID = secrets.KEY_ID
+AWS_SECRET_ACCESS_KEY = secrets.ACCESS_KEY          
+
+# Tell django-storages that when coming up with the URL for an item in S3
+# storage, keep it simple - just use this domain plus the path.
+# (If this isn't set, things get complicated). This controls how the
+# `static` template tag from `staticfiles` gets expanded, if you're using
+#it. We also use it in the next setting.  
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    
+# This is used by the `static` template tag from `static`, if you're using
+# that. Or if anything else refers directly to STATIC_URL. So it's safest
+# to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the 
+# collected static files (when you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
