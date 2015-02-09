68a433bff18d13e4431e048988b41ec200805d3d
akasem01 <kasemir.alex@gmail.com>
2015-02-09 14:46:58 -0500

    added user class to models

--- djangoapp/meows/models.py
+++ djangoapp/meows/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length = 25, unique= true)
+    owner_email = models.EmailField(max_length = 50, unique= true)
+    password = models.CharField(max_length = 50)
+    icon_URL = models.FilePathField(path = "")
+    active = models.BooleanField()
+    member_since = models.DateField
+    def __str__(self):
+        return self.username
+
+
