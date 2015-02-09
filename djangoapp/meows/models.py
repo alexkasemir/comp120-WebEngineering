b50722add39ef9fc31c940821675586f94ea40d2
akasem01 <kasemir.alex@gmail.com>
2015-02-09 15:15:27 -0500

    added other models and secrets

--- djangoapp/meows/models.py
+++ djangoapp/meows/models.py
@@ -4,10 +4,47 @@ class User(models.Model):
     username = models.CharField(max_length = 25, unique= true)
     owner_email = models.EmailField(max_length = 50, unique= true)
     password = models.CharField(max_length = 50)
-    icon_URL = models.FilePathField(path = "")
+    icon_URL = models.ImageField(upload_to = "", height_field = 50px, width_field = 50px)
     active = models.BooleanField()
-    member_since = models.DateField
+    member_since = models.DateField()
     def __str__(self):
         return self.username
 
+class User_Post(models.Model):
+    contains_image = models.BooleanField()
+    image_URL = models.ImageField(upload_to = "")
+    text_content = models.TextField(max_length = 255)
+    score = models.IntegerField()
+    time_created = models.DateField()
+    time_edited = models.DateField()
+    active = models.BooleanField()
+
+class Friends(models.Model):
+    user_id = models.ForeignKey(User)
+    follower_id = models.ForeignKey(User)
+
+class Posts(models.Model):
+    user_id = models.ForeignKey(User)
+    post_id = models.ForeignKey(User_Post)
+
+class Feedback(models.Model):
+    FEEDBACK_OPTIONS = (
+        ('l', 'like'),
+        ('d', 'dislike'),
+    )
+    post_id = models.ForeignKey(User_Post)
+    user_id = models.ForeignKey(User)
+    like_dislike = models.CharField(max_length=1, choices=FEEDBACK_OPTIONS)
+
+class Comment(models.Model):
+    user_id = models.ForeignKey(User)
+    post_id = models.ForeignKey(User_Post)
+    content = models.TextField(max_length = 255)
+
+class Album(models.Model):
+    name = models.CharField(max_length = 127)
+
+class Albums(models.Model):
+    album_id = models.ForeignKey(Album)
+    post_id = models.ForeignKey(User_Post)
 
--- djangoapp/purrer/settings.py
+++ djangoapp/purrer/settings.py
@@ -16,8 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(9tv$p4y!sk7$3%$c4c(!w@owr6ijj8+z-@30kvp1w$$ql6-w$'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
