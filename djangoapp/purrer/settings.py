d3af6d27a7741556ef3a01dcbdb28182b8ad9d09
Abdisalan <abdisalan.mohamud@tufts.edu>
2015-02-09 19:23:29 -0500

    amazon s3 is working

--- djangoapp/purrer/settings.py
+++ djangoapp/purrer/settings.py
@@ -116,3 +116,11 @@ STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 # Tell the staticfiles app to use S3Boto storage when writing the 
 # collected static files (when you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+STATICFILES_LOCATION = 'static'
+
+
+# CONFIGURATIONS FOR MEDIA FILES (FILES USERS UPLOAD)
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
