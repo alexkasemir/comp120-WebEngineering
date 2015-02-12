6e6a2f35851e83ed99b2876086f5a1963efc5f90
nusoff01 <nicholas.usoff@tufts.edu>
2015-02-11 20:26:39 -0500

    changed views to include templates

--- djangoapp/meows/views.py
+++ djangoapp/meows/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
-
+from django.template import RequestContext, loader
 from meows.models import User_Post
 
 def index(request):
     latest_posts = User_Post.objects.order_by('time_created')[:5]
-    output = ', '.join([p.text_content for p in latest_posts])
-    return HttpResponse(output)
-
+    template = loader.get_template('meows/index.html')
+    context = RequestContext(request, {
+        'latest_posts': latest_posts,
+    })
+    return HttpResponse(template.render(context))
--- djangoapp/purrer/settings.py
+++ djangoapp/purrer/settings.py
@@ -99,21 +99,21 @@ AWS_HEADERS = {
 
 AWS_STORAGE_BUCKET_NAME = secrets.BUCKET_NAME
 AWS_ACCESS_KEY_ID = secrets.KEY_ID
-AWS_SECRET_ACCESS_KEY = secrets.ACCESS_KEY          
+AWS_SECRET_ACCESS_KEY = secrets.ACCESS_KEY
 
 # Tell django-storages that when coming up with the URL for an item in S3
 # storage, keep it simple - just use this domain plus the path.
 # (If this isn't set, things get complicated). This controls how the
 # `static` template tag from `staticfiles` gets expanded, if you're using
-#it. We also use it in the next setting.  
+#it. We also use it in the next setting.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-    
+
 # This is used by the `static` template tag from `static`, if you're using
 # that. Or if anything else refers directly to STATIC_URL. So it's safest
 # to always set it.
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
-# Tell the staticfiles app to use S3Boto storage when writing the 
+# Tell the staticfiles app to use S3Boto storage when writing the
 # collected static files (when you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
