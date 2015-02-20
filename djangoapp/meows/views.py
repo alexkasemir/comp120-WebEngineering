172ba90a38f5bc2219faf5930d9ade481f518fd4
nusoff01 <nicholas.usoff@tufts.edu>
2015-02-20 13:18:39 -0500

    merging

---
+++ djangoapp/meows/views.py
@@ -0,0 +1,46 @@
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from meows.models import User_Post
+from django.http import HttpResponseRedirect
+from purrer.settings import MEDIA_ROOT
+
+from django.http import Http404
+from django.shortcuts import render
+
+from django.core.urlresolvers import reverse
+
+
+def index(request):
+    latest_posts = User_Post.objects.order_by('-time_created')[:10]
+    template = loader.get_template('meows/Pages/index.html')
+    context = RequestContext(request, {
+        'latest_posts': latest_posts,
+    })
+    return HttpResponse(template.render(context))
+
+
+
+def detail(request, user_post_id):
+    try:
+        user_post = User_Post.objects.get(pk=user_post_id)
+    except User_Post.DoesNotExist:
+        raise Http404("Post does not exist!")
+    return render(request, 'meows/Pages/details.html', {'user_post': user_post})
+
+def new_post(request):
+    return render(request, 'meows/Pages/new_post.html')
+
+def handle_uploaded_file(f):
+    dest = open(MEDIA_ROOT + '/images/' + f.name, 'wb+')
+    for chunk in f.chunks():
+        dest.write(chunk)
+
+def create_post(request):
+    print(request.FILES)
+    image = request.FILES['image_URL']
+    user_post = User_Post.create(request.POST.get('text_content'))
+    handle_uploaded_file(image)
+    user_post.image_URL = image.name
+    print(user_post)
+    user_post.save()
+    return render(request, 'meows/Pages/details.html', {'user_post': user_post})
---
+++ djangoapp/purrer/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'purrer.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^meows/', include('meows.urls', namespace="meows")),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+)
+
+urlpatterns += patterns('', (
+        r'^static/(?P<path>.*)$',
+        'django.views.static.serve',
+        {'document_root': 'media'}
+))
+
+urlpatterns += patterns('', (
+        r'^media/(?P<path>.*)$',
+        'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}
+)) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
