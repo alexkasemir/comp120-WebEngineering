4936148ec18e62c2afcd0e5199d15ed6f1c1c01d
Alexander Karys <akarys92@resnet160-202.medford.tufts.edu>
2015-04-01 18:37:55 -0400

    Added new user model

--- meows/models.py
+++ meows/models.py
@@ -1,15 +1,69 @@
 from django.db import models
 from django import forms
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 
-class User(models.Model):
-    username = models.CharField(max_length = 25, unique= True)
-    owner_email = models.EmailField(max_length = 50, unique= True)
-    password = models.CharField(max_length = 50)
-    icon_URL = models.ImageField()
-    active = models.BooleanField(default = True)
-    member_since = models.DateTimeField(auto_now_add = True)
-    def __str__(self):
-        return self.username
+
+#class User(models.Model):
+#    username = models.CharField(max_length = 25, unique= True)
+#    owner_email = models.EmailField(max_length = 50, unique= True)
+#    password = models.CharField(max_length = 50)
+#    icon_URL = models.ImageField()
+#    active = models.BooleanField(default = True)
+#    member_since = models.DateTimeField(auto_now_add = True)
+#    def __str__(self):
+#        return self.username
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('User must have a valid email address')
+        UN = self.normalize_email(email)
+        if kwargs.get('username'):
+            UN = kwargs.get('username')
+
+        account = self.model(
+            email = self.normalize_email(email), username=UN
+        )
+
+        account.set_password(password)
+        account.save()
+        
+        return account
+
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
+        account.is_admin = True
+        account.save()
+
+        return account
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
+
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    member_since = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQURIED_FIELDS = ['username',]
+
+    def _unicode_(self):
+        return self.email
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+
+    def get_short_name(self):
+        return self.first_name
+
 
 class User_Post(models.Model):
     contains_image = models.BooleanField(default = False)
